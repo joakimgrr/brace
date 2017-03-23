5e32bed5eb1abf9904937757ecfa21d2e203b694
Joakim Granbohm <joakim.granbohm@sysart.fi>
2017-03-23 16:23:46 +0200

    create middlewares and handle caching

---
+++ middleware.js
@@ -0,0 +1,99 @@
+const Lokka = require('lokka').Lokka
+const Transport = require('lokka-transport-http').Transport
+const fetch = require('node-fetch');
+
+const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+const WEATHER_URL = 'https://api.darksky.net/forecast';
+const HELSINKI = '60.1699,24.9384';
+
+const THIRTY_SECONDS = '30000';
+const HOUR = '3600000';
+
+const client = new Lokka({
+    transport: new Transport(HSL_GRAPHQL_URL)
+})
+
+require('dotenv').config()
+
+let timetableCache = {};
+let weatherCache = {};
+
+export function checkTimetableCache(req, res, next) {
+    const stopId = req.params.id;
+    console.log('Checking timetable cache for: ', stopId)
+    const now = new Date().getTime();
+
+    if( timetableCache[stopId] &&
+        timetableCache[stopId].data &&
+        (timetableCache[stopId].fetchedAt + THIRTY_SECONDS > now)
+    ) {
+        res.json(timetableCache[stopId].data)
+    } else {
+        next()
+    }
+}
+
+export function checkWeatherCache(req, res, next) {
+    console.log('Checking weather cache')
+    const now = new Date().getTime();
+
+    if( weatherCache.data &&
+        (weatherCache.fetchedAt + HOUR > now)
+    ) {
+        res.json(weatherCache.data)
+    } else {
+        next()
+    }
+}
+
+export function fetchTimetable(req, res, next) {
+    const stopId = req.params.id;
+    console.log('fetching timetable for: ', stopId)
+
+    client.query(`
+        {
+            stop(id: "${stopId}") {
+                stoptimesWithoutPatterns(timeRange: 10000) {
+                    trip {
+                        gtfsId,
+                        tripHeadsign,
+                        route {
+                            shortName
+                        }
+                    },
+                    realtimeArrival,
+                    realtime,
+                    scheduledArrival,
+                    serviceDay
+                }
+            }
+        }
+        `)
+        .then(response => {
+            const data = response.stop.stoptimesWithoutPatterns;
+
+            timetableCache[stopId] = {
+                data,
+                fetchedAt: new Date().getTime()
+            }
+
+            res.json(data)
+        });
+}
+
+export function fetchWeather(req, res, next) {
+    const fetchUrl = `${WEATHER_URL}/${process.env.DARK_SKY_API_KEY}/${HELSINKI}?units=si`
+
+    fetch(fetchUrl)
+        .then(result => {
+            return result.json()
+        })
+        .then(data => {
+            weatherCache = {
+                data,
+                fetchedAt: new Date().getTime()
+            }
+
+            res.json(data)
+        })
+}
--- server.js
+++ server.js
@@ -1,79 +1,15 @@
-const Lokka = require('lokka').Lokka
-const Transport = require('lokka-transport-http').Transport
 const express = require('express');
-const fetch = require('node-fetch');
 const app = express();
 
-require('dotenv').config()
+const mw = require('./middleware')
 
-const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
-const WEATHER_URL = 'https://api.darksky.net/forecast';
-const HELSINKI = '60.1699,24.9384';
+const checkTimetableCache = mw.checkTimetableCache;
+const fetchTimetable = mw.fetchTimetable;
+const checkWeatherCache = mw.checkWeatherCache;
+const fetchWeather = mw.fetchWeather;
 
-const client = new Lokka({
-    transport: new Transport(HSL_GRAPHQL_URL)
-})
+app.get('/timetable/:id', checkTimetableCache, fetchTimetable);
 
-let timetableCache = {};
-let weatherDataCache = null;
-
-app.get('/timetable/:id', async (req, res) => {
-    const timetableData = await fetchTimetable(req.params.id);
-
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    res.json(timetableData);
-})
-
-app.get('/weather', async (req, res) => {
-    console.log('weather')
-    const weatherData = await fetchWeatherData();
-
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    res.json(weatherData);
-})
+app.get('/weather', checkWeatherCache, fetchWeather);
 
 app.listen(3000);
-
-async function fetchWeatherData() {
-    const fetchUrl = `${WEATHER_URL}/${process.env.DARK_SKY_API_KEY}/${HELSINKI}?units=si`
-    return weatherDataCache ? weatherDataCache : fetch(fetchUrl)
-        .then(res => {
-            let data = res.json();
-            weatherDataCache = data
-            return data;
-        });
-}
-
-async function fetchTimetable(stopId) {
-    return timetableCache[stopId] ? timetableCache[stopId] : client.query(`
-        {
-            stop(id: "${stopId}") {
-                stoptimesWithoutPatterns(timeRange: 10000) {
-                    trip {
-                        gtfsId,
-                        tripHeadsign,
-                        route {
-                            shortName
-                        }
-                    },
-                    realtimeArrival,
-                    realtime,
-                    scheduledArrival,
-                    serviceDay
-                }
-            }
-        }
-        `)
-        .then(response => {
-            const timetables = response.stop.stoptimesWithoutPatterns;
-
-            timetableCache[stopId] = timetables;
-            return timetables
-        });
-}
